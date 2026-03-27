fd968df5c2dabd80411cc7b026728f95fd36c1ba
BairagoniVaishnavi <bairagonivaishnavi11@gmail.com>
2026-03-27 17:13:13 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL:'https://fitsense-backend.onrender.com/api',
+  baseURL:'https://fitsense-backend.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
