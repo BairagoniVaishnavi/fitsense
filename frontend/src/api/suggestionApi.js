a7940982f421f051d00a5f781194323259e183e3
BairagoniVaishnavi <bairagonivaishnavi11@gmail.com>
2026-03-30 14:29:30 +0530

    Normalize suggestion API response handling

--- frontend/src/api/suggestionApi.js
+++ frontend/src/api/suggestionApi.js
@@ -1,11 +1,15 @@
 import api from './axios'
 
+const normalize = (res) => {
+  return res?.data || res?.plan || res?.suggestion || res || null
+}
+
 export const getSuggestion = async (payload) => {
   const { data } = await api.post('/suggestion', payload)
-  return data
+  return normalize(data)
 }
 
 export const getSuggestionTypes = async () => {
   const { data } = await api.get('/suggestion/types')
-  return data
+  return data?.data || data
 }
